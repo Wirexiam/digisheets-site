a49c1839451a1978ea68fd43d6a06e24fbceb7f8
Wirexiam <alfe2230@gmail.com>
2025-08-13 16:09:17 +0300

    .

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -126,9 +126,9 @@ function build() {
 
   const builtAt = new Date().toISOString();
   const xml = [
-    `<!--  built at ${builtAt}  -->`,
-    '<?xml version="1.0" encoding="UTF-8"?>',
-    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+  '<?xml version="1.0" encoding="UTF-8"?>',
+  `<!--  built at ${builtAt}  -->`,
+  '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
   ]
     .concat(items.map(it => {
       const extra =
